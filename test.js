d5eca5cd422747a5be44c314e1ed8e61d1cac325
Erbol S <m.erbol.sultanbayev@gmail.com>
2026-02-27 00:56:55 +0500

    feat: add CI pipeline

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('Starting tests...');
+setTimeout(() => console.log('Waiting 3 seconds...'), 3000);
+setTimeout(() => console.log('Waiting for tests...'), 2000);
+console.log('Tests completed!');
